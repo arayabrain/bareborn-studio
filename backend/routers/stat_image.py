de974b83ff72e9e2c109d89afc6ac62e99df6f2d
quannd <quannd@reactplus.jp>
2023-05-24 17:27:01 +0700

    create load, save stat image param API

---
+++ backend/routers/stat_image.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from firebase_admin import auth
+
+from backend.deps import get_current_user
+from backend.models.stat_image import StatImageParam
+from backend.models.user import User
+
+router = APIRouter()
+
+
+@router.get("", response_model=StatImageParam)
+async def get_stat_image_param(current_user: User = Depends(get_current_user)):
+    user = auth.get_user(current_user['uid'])
+    current_claims = user.custom_claims
+    return StatImageParam(**current_claims)
+
+
+@router.post("", response_model=StatImageParam)
+async def set_stat_image_param(
+    data: StatImageParam, current_user: User = Depends(get_current_user)
+):
+    user = auth.get_user(current_user['uid'])
+    current_claims = user.custom_claims
+    current_claims.update(data.dict())
+    auth.set_custom_user_claims(user.uid, current_claims)
+    return StatImageParam(**current_claims)
