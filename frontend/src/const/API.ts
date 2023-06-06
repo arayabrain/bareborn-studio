27ccd0f1372ee2ddd09ecaf25bd59f0acbad3ca0
LinhPb <linhpb@react-plus.com>
2023-06-06 10:53:38 +0700

    fix env

--- frontend/src/const/API.ts
+++ frontend/src/const/API.ts
@@ -2,6 +2,6 @@ const HOST = process.env.REACT_APP_SERVER_HOST
 const PORT = process.env.REACT_APP_SERVER_PORT
 const HOST_DATABASE = process.env.REACT_APP_SERVER_DATABASE_HOST
 
-export const BASE_URL = `http://${HOST}:${PORT}`
+export const BASE_URL = `https://${HOST}`
 export const WS_BASE_URL = `ws://${HOST}:${PORT}`
 export const DATABASE_URL_HOST = `${HOST_DATABASE}`
