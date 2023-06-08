a16c84cff8b2f2729bed522f779a065838c66e04
LinhPb <linhpb@react-plus.com>
2023-06-08 16:00:18 +0700

    Change env

--- frontend/src/const/API.ts
+++ frontend/src/const/API.ts
@@ -2,7 +2,6 @@ const HOST = process.env.REACT_APP_SERVER_HOST
 const PORT = process.env.REACT_APP_SERVER_PORT
 const HOST_DATABASE = process.env.REACT_APP_SERVER_DATABASE_HOST
 
-// export const BASE_URL = `https://${HOST}`
-export const BASE_URL = `https://internal-aa-mri.waai.io`
+export const BASE_URL = `https://${HOST}:${PORT}`
 export const WS_BASE_URL = `ws://${HOST}:${PORT}`
 export const DATABASE_URL_HOST = `${HOST_DATABASE}`
