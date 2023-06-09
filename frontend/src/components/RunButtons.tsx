47e77c5d94a79ee5b24c27a9bd5502d521e649e6
SangLv <sanglv@reactplus.jp>
2023-06-09 13:35:13 +0700

    Remove allowRun

--- frontend/src/components/RunButtons.tsx
+++ frontend/src/components/RunButtons.tsx
@@ -25,10 +25,7 @@ import {
   RUN_BTN_OPTIONS,
   RUN_BTN_TYPE,
 } from 'store/slice/Pipeline/PipelineType'
-import {
-  selectAllowRunButton,
-  selectPipelineRunBtn,
-} from 'store/slice/Pipeline/PipelineSelectors'
+import { selectPipelineRunBtn } from 'store/slice/Pipeline/PipelineSelectors'
 import { setRunBtnOption } from 'store/slice/Pipeline/PipelineSlice'
 
 export const RunButtons = React.memo<UseRunPipelineReturnType>((props) => {
@@ -45,7 +42,6 @@ export const RunButtons = React.memo<UseRunPipelineReturnType>((props) => {
   const dispatch = useDispatch()
 
   const runBtnOption = useSelector(selectPipelineRunBtn)
-  const allowRunButton = useSelector(selectAllowRunButton)
 
   const [dialogOpen, setDialogOpen] = React.useState(false)
   const { enqueueSnackbar } = useSnackbar()
@@ -107,7 +103,7 @@ export const RunButtons = React.memo<UseRunPipelineReturnType>((props) => {
         }}
         variant="contained"
         ref={anchorRef}
-        disabled={isStartedSuccess || !allowRunButton}
+        disabled={isStartedSuccess}
       >
         <Button onClick={handleClick}>{RUN_BTN_LABELS[runBtnOption]}</Button>
         <Button size="small" onClick={handleToggle}>
