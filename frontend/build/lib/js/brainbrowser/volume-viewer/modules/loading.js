960f8b58f03db69ea52256f5fea41d0bff8fc4a9
tienday <tienday@local.local>
2023-06-12 14:44:29 +0900

    build frontend.

--- frontend/build/lib/js/brainbrowser/volume-viewer/modules/loading.js
+++ frontend/build/lib/js/brainbrowser/volume-viewer/modules/loading.js
@@ -435,7 +435,7 @@ BrainBrowser.VolumeViewer.modules.loading = function (viewer) {
       )
       volume.propagateEventTo('*', viewer)
       ;['xspace', 'yspace', 'zspace'].forEach(function (axis) {
-        volume.position[axis] = Math.floor(volume.header[axis].space_length / 2)
+        volume.position[axis] = 0
       })
 
       volume.display.forEach(function (panel) {
