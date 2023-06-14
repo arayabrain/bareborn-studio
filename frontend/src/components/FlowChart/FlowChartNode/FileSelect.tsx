c1789e41da1d165387fa0c3db52193dfff1a347a
SangLv <sanglv@reactplus.jp>
2023-06-14 17:57:43 +0700

    remove comment code

--- frontend/src/components/FlowChart/FlowChartNode/FileSelect.tsx
+++ frontend/src/components/FlowChart/FlowChartNode/FileSelect.tsx
@@ -1,13 +1,10 @@
-import React, { useContext /*useEffect*/ } from 'react'
+import React, { useContext } from 'react'
 import { Button, Typography } from '@mui/material'
 import ButtonGroup from '@mui/material/ButtonGroup'
 
 import { FILE_TREE_TYPE } from 'api/files/Files'
 import { LinearProgressWithLabel } from './LinerProgressWithLabel'
-import {
-  AlignmentData,
-  FILE_TYPE,
-} from 'store/slice/InputNode/InputNodeType'
+import { AlignmentData, FILE_TYPE } from 'store/slice/InputNode/InputNodeType'
 import { useFileUploader } from 'store/slice/FileUploader/FileUploaderHook'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { DialogContext } from '../DialogContext'
@@ -20,13 +17,8 @@ export const FileSelect = React.memo<{
   nodeId?: string
   onChangeFilePath: (path: string | string[]) => void
 }>(({ multiSelect = false, filePath, nodeId, fileType, onChangeFilePath }) => {
-  const {
-    onUploadFile,
-    pending,
-    uninitialized,
-    progress,
-    error,
-  } = useFileUploader({ fileType, nodeId })
+  const { onUploadFile, pending, uninitialized, progress, error } =
+    useFileUploader({ fileType, nodeId })
   const onUploadFileHandle = (formData: FormData, fileName: string) => {
     onUploadFile(formData, fileName)
   }
@@ -70,50 +62,13 @@ export const FileSelectImple = React.memo<FileSelectImpleProps>(
   ({ filePath, nodeId }) => {
     const navigate = useNavigate()
     const [searchParams] = useSearchParams()
-    const { onOpenImageAlignment /*images*/ } = useContext(DialogContext)
-    // const selectInput = useSelector(selectInputNodeById(nodeId || ''))
-    // const dispatch = useDispatch()
+    const { onOpenImageAlignment } = useContext(DialogContext)
 
     const inputNode = useSelector(
       selectInputNodeParam(nodeId || ''),
     ) as AlignmentData
 
     const id = searchParams.get('id')
-
-    // useEffect(() => {
-    //   if (!nodeId) return
-    //   const valueAlignments = (selectInput.param as AlignmentData)?.alignments
-    //     ?.value
-    //   let newParams: Params[] = []
-    //   if (Array.isArray(filePath)) {
-    //     newParams = filePath.map((path: string) => {
-    //       const image = images.find((image) => image.url === path)
-    //       const param = valueAlignments?.find(
-    //         (align) => align.image_id === image?.id,
-    //       )
-    //       if (param) return param
-    //       return {
-    //         image_id: image?.id || path,
-    //         x_pos: 0,
-    //         x_resize: 1,
-    //         x_rotate: 0,
-    //         y_pos: 0,
-    //         y_resize: 1,
-    //         y_rotate: 0,
-    //         z_pos: 0,
-    //         z_resize: 1,
-    //         z_rotate: 0,
-    //       }
-    //     })
-    //   }
-    //   dispatch(
-    //     setInputNodeParamAlignment({
-    //       nodeId,
-    //       param: newParams,
-    //     }),
-    //   )
-    //   //eslint-disable-next-line
-    // }, [filePath, nodeId, JSON.stringify(images)])
 
     const getNameSelectec = () => {
       if (Array.isArray(filePath)) {
