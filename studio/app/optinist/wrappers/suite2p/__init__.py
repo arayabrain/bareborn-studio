0b6f37b710722ac34651a0d048f380d2c93315d2
ReiHashimoto <42664619+ReiHashimoto@users.noreply.github.com>
2023-11-28 09:09:58 +0900

    apply to suite2p

--- studio/app/optinist/wrappers/suite2p/__init__.py
+++ studio/app/optinist/wrappers/suite2p/__init__.py
@@ -1,24 +1,24 @@
-from studio.app.optinist.wrappers.suite2p.file_convert import suite2p_file_convert
-from studio.app.optinist.wrappers.suite2p.registration import suite2p_registration
-from studio.app.optinist.wrappers.suite2p.roi import suite2p_roi
-from studio.app.optinist.wrappers.suite2p.spike_deconv import suite2p_spike_deconv
+from studio.app.optinist.wrappers.suite2p.file_convert import Suite2pFileConvert
+from studio.app.optinist.wrappers.suite2p.registration import Suite2pRegistration
+from studio.app.optinist.wrappers.suite2p.roi import Suite2pRoi
+from studio.app.optinist.wrappers.suite2p.spike_deconv import Suite2pSpikeDeconv
 
 suite2p_wrapper_dict = {
     "suite2p": {
         "suite2p_file_convert": {
-            "function": suite2p_file_convert,
+            "function": Suite2pFileConvert,
             "conda_name": "suite2p",
         },
         "suite2p_registration": {
-            "function": suite2p_registration,
+            "function": Suite2pRegistration,
             "conda_name": "suite2p",
         },
         "suite2p_roi": {
-            "function": suite2p_roi,
+            "function": Suite2pRoi,
             "conda_name": "suite2p",
         },
         "suite2p_spike_deconv": {
-            "function": suite2p_spike_deconv,
+            "function": Suite2pSpikeDeconv,
             "conda_name": "suite2p",
         },
     }
--- studio/app/optinist/wrappers/suite2p/registration.py
+++ studio/app/optinist/wrappers/suite2p/registration.py
@@ -1,38 +1,73 @@
+from typing import List
+
+from pydantic import BaseModel
+from pydantic.dataclasses import Field
+
+from studio.app.common.core.algo import AlgoTemplate
 from studio.app.common.dataclass import ImageData
 from studio.app.optinist.dataclass import Suite2pData
 
 
-def suite2p_registration(
-    ops: Suite2pData, output_dir: str, params: dict = None, **kwargs
-) -> dict(ops=Suite2pData):
-    from suite2p import default_ops, registration
+class RegistrationParams(BaseModel):
+    frames_include: int = Field(-1)
+    keep_movie_raw: bool = Field(False)
+    do_bidiphase: bool = Field(False)
 
-    function_id = output_dir.split("/")[-1]
-    print("start suite2p registration:", function_id)
+    smooth_sigma: float = Field(1.15)
+    smooth_sigma_time: int = Field(0)
+    bidiphase: int = Field(0)
+    maxregshift: float = Field(0.1)
+    maxregshiftNR: int = Field(5)
+    nonrigid: bool = Field(True)
+    block_size: List[int] = Field([128, 128])
+    snr_thresh: float = Field(1.2)
+    functional_chan: int = Field(1)
+    align_by_chan: int = Field(1)
+    reg_tif: bool = Field(False)
+    th_badframes: float = Field(1.0)
+    diameter: int = Field(0)
+    one_Preg: bool = Field(False)  # exact name is 1Preg, but it is not allowed
+    spatial_hp_reg: int = Field(42)
+    pre_smooth: int = Field(0)
+    spatial_taper: int = Field(40)
+    bidi_corrected: bool = Field(False)
 
-    ops = ops.data
-    refImg = ops["meanImg"]
-    print("start suite2_registration")
 
-    # REGISTRATION
-    if len(refImg.shape) == 3:
-        refImg = refImg[0]
+class Suite2pRegistration(AlgoTemplate):
+    def run(
+        self, params: RegistrationParams, ops: Suite2pData
+    ) -> dict(ops=Suite2pData):
+        from suite2p import default_ops, registration
 
-    ops = {**default_ops(), **ops, **params}
+        print("start suite2p registration:", self.function_id)
 
-    # register binary
-    ops = registration.register_binary(ops, refImg=refImg)
+        ops = ops.data
+        refImg = ops["meanImg"]
+        print("start suite2_registration")
 
-    # compute metrics for registration
-    if ops.get("do_regmetrics", True) and ops["nframes"] >= 1500:
-        ops = registration.get_pc_metrics(ops)
+        # REGISTRATION
+        if len(refImg.shape) == 3:
+            refImg = refImg[0]
 
-    info = {
-        "refImg": ImageData(ops["refImg"], output_dir=output_dir, file_name="refImg"),
-        "meanImgE": ImageData(
-            ops["meanImgE"], output_dir=output_dir, file_name="meanImgE"
-        ),
-        "ops": Suite2pData(ops, file_name="ops"),
-    }
+        one_Preg = params["one_Preg"]
+        params["1Preg"] = one_Preg
+        ops = {**default_ops(), **ops, **params}
 
-    return info
+        # register binary
+        ops = registration.register_binary(ops, refImg=refImg)
+
+        # compute metrics for registration
+        if ops.get("do_regmetrics", True) and ops["nframes"] >= 1500:
+            ops = registration.get_pc_metrics(ops)
+
+        info = {
+            "refImg": ImageData(
+                ops["refImg"], output_dir=self.output_dir, file_name="refImg"
+            ),
+            "meanImgE": ImageData(
+                ops["meanImgE"], output_dir=self.output_dir, file_name="meanImgE"
+            ),
+            "ops": Suite2pData(ops, file_name="ops"),
+        }
+
+        return info
